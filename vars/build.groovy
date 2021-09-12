046f5c29ec74989d16860f42eb6945a3fbd2a092
Sumeena Farheen <sumeena.farheen@ust.com>
2021-09-12 21:31:58 +0530

    build

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,8 +1,9 @@
 def call(String BUILD_TYPE) {
     node {
         sh "echo 'Performing Action - ${BUILD_TYPE}'"
-		sh "chmod +x mvnw"
+        sh "ls -ltr"
+		sh "chmod +x ../mvnw"
 		sh "ls -ltr"
-		sh "./mvnw ${BUILD_TYPE}"
+		sh "../mvnw ${BUILD_TYPE}"
     }
 }
